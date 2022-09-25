39a1ec55526915a8d1b4457aee90506f1ed4566b
Victor <victor.cruz@rappi.com>
2022-09-25 18:41:23 -0500

    Init Reactive Spring Boot

---
+++ src/main/java/com/example/reactivespringcrud/ReactiveSpringCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactivespringcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSpringCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveSpringCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/reactivespringcrud/ReactiveSpringCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactivespringcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveSpringCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
